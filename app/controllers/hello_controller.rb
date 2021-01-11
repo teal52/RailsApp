60074f2e2cdb4697c3983812c3ba67e28b92cfc4
teal <mys.alter10@gmail.com>
2021-01-11 15:58:47 +0900

    first commit

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,14 @@
+class HelloController < ApplicationController
+  def index
+    if params['msg'] != nil then
+      @title = params['msg']
+    else
+      @title = 'index'
+    end
+    @msg = 'this is redirect sample...'
+
+    def other
+      redirect_to action: :index, params: {'msg': 'from other page'}
+    end
+  end
+end
