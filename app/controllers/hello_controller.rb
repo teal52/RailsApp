4f46c2b13cae28b24c568536d46be3fa372dd195
teal <mys.alter10@gmail.com>
2021-04-15 00:10:43 +0900

    make  new layout files

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -3,14 +3,17 @@ class HelloController < ApplicationController
   def index
     if request.post? then
       @title = 'Result'
-      if params['check1'] then
-        @msg = 'you checked!'
+      if params['s1'] then
+        @msg = 'you selected: '
+        for val in params['s1']
+          @msg += val + ' '
+        end
       else
-        @msg = 'not checked...'
+        @msg = 'not selected...'
       end
     else
       @title = 'index'
-      @msg = 'checked it ...'
+      @msg = 'select List...'
     end
   end
 
